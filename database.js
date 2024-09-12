e5524813c2995db0ecd8736373bed73f94c45ffd
lekkla <platformagencydeveloper@gmail.com>
2024-09-12 22:03:48 +0700

    dd

--- database.js
+++ database.js
@@ -33,7 +33,7 @@ const firebaseConfig = {
     try {
        // let numberOfElements = await db.count(`/transaction/${userid}`);
         const d = new Date();
-        d = new Date(d.getTime() - 3000000);
+        d = new Date(d.getTime());
         let textDate= d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
 console.log(textDate);
 
